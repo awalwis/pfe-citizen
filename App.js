c90796533c2112deb4ffe5509e84f44313456f37
maicie-byte <merlin.duvivier@student.vinci.be>
2020-12-08 20:08:39 +0100

    lastCommit 8/12

--- App.js
+++ App.js
@@ -5,17 +5,21 @@ import Camera from './component/Camera'
 
 export default function App() {
   const [doitScanner, setDoitScanner] = useState(false);
-
+  const [name, setName] = useState("");
+  const [nameEnter,setNameEnter] = useState(false);
+  const [newName, setNewName] = useState("");
+  
 
   return (
     <View
       style={{
         flex: 1,
         flexDirection: 'column',
-        justifyContent: 'flex-end',
+        justifyContent: 'center',
       }}>
         {doitScanner && <Camera></Camera>}
-        {!doitScanner && <Button title={'to Scan Again'} onPress={() => setDoitScanner(true)}/>}
+        {!doitScanner && <Button title={'tap to Scan'} onPress={() => setDoitScanner(true)}/>}
+        {doitScanner && <Button title={'tap to descan'} onPress={()=> setDoitScanner(false)}/>}
     </View>
   );
 }
