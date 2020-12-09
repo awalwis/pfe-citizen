b380a962e3ce3eeb85b24212c9d887b58b81cdf0
maicie-byte <merlin.duvivier@student.vinci.be>
2020-12-09 11:45:40 +0100

    commit entré nom

--- App.js
+++ App.js
@@ -1,14 +1,30 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import { Text, View, StyleSheet, Button,TextInput } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 import Camera from './component/Camera'
-
+const CreateNew = (props)=>{
+  const [name , setName] = useState('')
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    props.addNew(name)
+  }
+  return(
+    <View>
+      <Text>create a new name</Text>
+      <TextInput onChangeText={text=>setName(text)}></TextInput>
+      <Button title={'confirmer le nom'} onPress={handleSubmit}></Button>
+    </View>
+  )
+}
 export default function App() {
   const [doitScanner, setDoitScanner] = useState(false);
   const [name, setName] = useState("");
   const [nameEnter,setNameEnter] = useState(false);
-  const [newName, setNewName] = useState("");
   
+  const addNew = (nom) =>{
+    setName(nom)
+    setNameEnter(true);
+  }
 
   return (
     <View
@@ -17,9 +33,11 @@ export default function App() {
         flexDirection: 'column',
         justifyContent: 'center',
       }}>
+        {!nameEnter && <CreateNew addNew={addNew}></CreateNew>}
+        <Text>{name}</Text>
         {doitScanner && <Camera></Camera>}
-        {!doitScanner && <Button title={'tap to Scan'} onPress={() => setDoitScanner(true)}/>}
-        {doitScanner && <Button title={'tap to descan'} onPress={()=> setDoitScanner(false)}/>}
+        {!doitScanner && nameEnter && <Button title={'tap to Scan'} onPress={() => setDoitScanner(true)}/>}
+        {doitScanner && <Button title={'stop scan'} onPress={()=> setDoitScanner(false)}/>}
     </View>
   );
 }
